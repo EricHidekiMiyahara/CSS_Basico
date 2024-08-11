7bcfbfa3e3add946c593ae9c65ece810c953b915
Eric Miyahara <erichidekimiyahara@gmail.com>
2024-08-10 21:37:52 -0300

    Update index.js

--- aulas-js/js/index.js
+++ aulas-js/js/index.js
@@ -1,1 +1,38 @@
-document.getElementById()
+//Inserindo um texto no paragrafo
+/* Autor: Eric
+    JS
+*/
+
+//DECLARACAO DE VARIAVEIS
+var nome, sobrenome, nomeCompleto, idade, soma;
+
+// ATRIBUICAO DOS VALORES
+nome="Eric";
+sobrenome="Miyahara";
+nomeCompleto = nome + " " + sobrenome;
+idade = 21;
+
+soma = idade + 10;
+pessoa = soma+nome;
+
+
+document.getElementById("texto").innerHTML = pessoa;
+
+var x = 10; //aqui é 10
+
+{
+    var x = 2; //aqui é 2
+}
+
+alert(x);
+
+/* let x = 10 //aqui é 10
+
+{
+    let x = 2; //aqui é 2
+}
+
+//aqui é 10
+document.getElementById("texto").inner.HTML = x; 
+
+*/

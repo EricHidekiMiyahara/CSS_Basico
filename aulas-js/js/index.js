2560557a897358198fcaec75835ddd58c4c2f642
Eric Miyahara <erichidekimiyahara@gmail.com>
2024-08-14 17:48:43 -0300

    Started objetos

--- aulas-js/js/index.js
+++ aulas-js/js/index.js
@@ -1,3 +1,11 @@
-function minhaFuncao(){
-    var x = 2;
+const carro = {
+    marca: "ford", 
+    modelo: "ka", 
+    ano:2001, placa:"ABC-1234",
+    buzina: function() {alert('biiiii')},
+    completo: function(){
+        return "A marca é " + this.marca + ", o modelo é " + this.modelo + ", o ano é " + this.ano + " e a placa é " + this.placa;
+    }
 }
+
+console.log(carro.completo());
